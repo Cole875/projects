8860fa3b4b84fc95c6ecfd50c04392312748abc3
unknown <tambaro123@gmail.com>
2017-09-18 20:34:58 -0400

    uploading projects

---
+++ ProjectFreeLancer/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String args[]) {
+        //even fiz odd buzz
+        for(int i = 0; i < 100; i++){
+            if(i % 2 == 0){
+                System.out.println("fiz " + i);
+            }
+            else {
+                System.out.println("buzz " + i);
+            }
+        }
+    }
+}
