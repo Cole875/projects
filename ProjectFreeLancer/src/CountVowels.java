2f75dd40cb143e05697c2254cc5c58031bc55461
unknown <tambaro123@gmail.com>
2017-10-30 22:00:00 -0400

    fix CountVowels file

--- ProjectFreeLancer/src/CountVowels.java
+++ ProjectFreeLancer/src/CountVowels.java
@@ -1,6 +1,29 @@
-public class CountVowels {
-    public static void main (String hargs[]){
-        System.out.println("Hello")
+import javax.print.DocFlavor;
+import java.io.File;
+import java.util.Scanner;
 
+public class CountVowels {
+    public static void main (String args[]) {
+        String vowels = "T";
+        Scanner sc = null;
+        int count = 0;
+        try {
+            sc = new Scanner(new File("Tarzan.txt"));
+        }
+        catch (Exception e) {
+            System.out.println("Computer has failed to read Tarzan");
+            System.exit(1);
+        }
+        //sc.
+        while (sc.hasNextLine()) {
+            String line = sc.nextLine();
+            for (int i = 0; i < line.length(); i++){
+                String testedCharacter = line.charAt(i) + "";
+                if (vowels.contains(testedCharacter)) {
+                    count++;
+                }
+            }
+        }
+        System.out.println(count);
     }
 }
